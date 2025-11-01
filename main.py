92acdc140ab6628c4162ae269ea48d7a2e38c265
ShiZick820 <landiksasa@gmail.com>
2025-11-01 17:11:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from settings import *
 from sounds import load_sounds
 from keys import draw_keys, create_key_rects
 from ui.slider import Slider  # ⬅️
+from ui.button import Button
 
 init()
 screen = display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -19,6 +20,9 @@ font = font.SysFont(None, 25)
 # Створюємо слайдер гучності ⬅️
 volume_slider = Slider(50, 50, 200, min_value=0, max_value=100, font=font)
 
+dark_button = Button(screen.get_width() - 400, 20, 150, 40, 'DARK_THEME')
+light_button = Button(screen.get_width() - 230, 20, 150, 40, 'LIGHT_THEME')
+
 running = True
 while running:
     screen.fill(WHITE)
@@ -29,6 +33,9 @@ while running:
     # Малюємо слайдер і отримуємо значення гучності ⬅️
     volume_value = volume_slider.draw(screen)
     volume = volume_value / 100  # переводимо в діапазон 0.0 - 1.0
+
+    dark_button.draw(screen)
+    light_button.draw(screen)
 
     # Встановлюємо гучність для всіх звуків ⬅️
     for sound in sounds.values():
@@ -65,6 +72,12 @@ while running:
                 if rect.collidepoint(pos):
                     sounds[keys_list[i]].play()
                     pressed_keys.add(i)
+        if dark_button.is_clicked(e):
+            WHITE = (10, 10, 10)
+            volume_slider.text_color = (255, 255, 255)
+        if light_button.is_clicked(e):
+            WHITE = (200, 200, 200)
+            volume_slider.text_color = (0, 0, 0)
 
         if e.type == MOUSEBUTTONUP:
             pos = e.pos
